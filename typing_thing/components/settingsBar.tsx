44f2cbeee7d0df02dbfdeac478884f60a734283f
Josh Arbon <jarbon@student.neumont.edu>
2025-02-06 19:08:19 -0700

    added settings bar component

---
+++ typing_thing/components/settingsBar.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function SettingsBar(){
+    return (
+        <div className="h-20 flex items-center">
+            <input  placeholder={"username"} className="ml-5 text-white text-center bg-gray-600 border"/>
+        </div>
+    )
+}
