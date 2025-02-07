920142fd3bf3ff4ecfe7fa1cc13420f42f5b7484
Josh Arbon <jarbon@student.neumont.edu>
2025-02-06 19:11:05 -0700

    added settings button to home page

--- typing_thing/app/page.tsx
+++ typing_thing/app/page.tsx
@@ -1,8 +1,16 @@
-import Image from "next/image";
+"use client"
 import Button from "@/components/Button"
+import "@/styles/global.css"
+import { useState } from "react";
+import { SettingsButton } from "@/components/Button";
+import SettingsBar from "@/components/settingsBar";
 import Words from "@/components/Words"
 
 export default function Home() {
+  const [openSettings, openSettingsClicked] = useState(false)
+  const handleClick = () => {
+    openSettingsClicked(prev => !prev)
+  }
   const words = [
     "the","be","of","and","a","to",
     "in","he","have","it","that","for","they","I",
@@ -12,6 +20,17 @@ export default function Home() {
     "can","more","if","no","man","out","other"
   ]
   return (
+    <>
+      {
+        openSettings && (
+          <div className="absolute pt-7 pl-3 w-screen bg-gray-600">
+            <SettingsBar/>
+          </div>
+        )
+      }
+    <div className="absolute right-8 top-8" onClick={handleClick}>
+      <SettingsButton/>
+    </div>
     <div>
       <div className="h-screen flex items-center justify-center mr-10 ml-10">
         <Words words={words}>
@@ -19,5 +38,6 @@ export default function Home() {
         </Words>
         </div>
     </div>
+    </>
   )
 }
