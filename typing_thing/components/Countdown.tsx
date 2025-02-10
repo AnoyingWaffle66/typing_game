b352da48dd88317f7beeca74ef2db9e6889fa759
JayceeSochi <dcarnold22@gmail.com>
2025-02-10 16:06:21 -0700

    broken wpm placeholder stuff?

--- typing_thing/components/Countdown.tsx
+++ typing_thing/components/Countdown.tsx
@@ -5,18 +5,26 @@ import React, {useState, useEffect} from 'react';
 // TO-DO (Make it run on an event instead of right away)
 
 function Countdown() {
-    const [count, setCount] = useState(60);
+    const [wordCount, setWordCount] = useState(0);
+    const [count, setCount] = useState(0);
     const [isActive, setIsActive] = useState(false);
 
     // For Handling Keyboard Input
     useEffect(() => {
         const handleKeyPress = (event : KeyboardEvent) => {
-            if (event.key != null) {
+
+            // Sets Active
+            if (!isActive) {
                 setIsActive(true);
+            }
+
+            if (event.key == ' ' && count < 60) {
+                setWordCount(prevWordCount => prevWordCount + 1);
             }
         };
 
         window.addEventListener("keydown", handleKeyPress);
+
         return () => window.removeEventListener("keydown", handleKeyPress);
     }, []);
 
@@ -25,16 +33,19 @@ function Countdown() {
         // Keeps returning until a key has been pressed.
         if (!isActive) return;
 
+        if (count < 60) {
         const intervalId = setInterval(() => {
             setCount(prevCount => prevCount + 1);
         }, 1000);
 
         return () => clearInterval(intervalId);
+    }
     }, [isActive]);
 
     return (
         <div>
-            <progress value={(count - 60) / 60}/>
+            <h1>WPM: {Math.ceil(wordCount / (count / 60))}</h1>
+            <progress value={count / 60}/>
         </div>
     );
 }
