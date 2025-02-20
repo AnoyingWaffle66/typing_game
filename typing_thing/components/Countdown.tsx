f8b4fb2deead901e4ad64667ba13dbccbc847f4b
JayceeSochi <dcarnold22@gmail.com>
2025-02-20 16:11:36 -0700

    Bigger bar :)

--- typing_thing/components/Countdown.tsx
+++ typing_thing/components/Countdown.tsx
@@ -7,11 +7,11 @@ import React, {useState, useEffect} from 'react';
 function Countdown() {
     // const [wordCount, setWordCount] = useState(0);
     const [count, setCount] = useState(0);
+    const [smoothCount, setSmoothCount] = useState(0);
     const [isActive, setIsActive] = useState(false);
 
     const resetTimer = () => {
         setCount(0)
-        setWordCount(0)
         setIsActive(false)
     }
     window.addEventListener('storage', resetTimer)
@@ -39,18 +39,33 @@ function Countdown() {
         // Keeps returning until a key has been pressed.
         if (!isActive) return;
 
+        if (count >= 60 && isActive) {
+            setIsActive(false);
+        }
+
         if (count < 60) {
         const intervalId = setInterval(() => {
             setCount(prevCount => prevCount + 1);
         }, 1000);
         return () => clearInterval(intervalId);
-    }
+    } 
+    }, [isActive]);
+
+    useEffect(() => {
+        if (!isActive) return;
+
+        if (count < 60) {
+            const intervalId = setInterval(() => {
+                setSmoothCount(prevSmoothCount => prevSmoothCount + 1);
+            }, 5);
+            return () => clearInterval(intervalId);
+        }
     }, [isActive]);
 
     return (
         <div>
             <h1>WPM: {count <= 0 ? "Start typing" : Math.ceil(Number(localStorage.getItem('correctSpaces')) / (count / 60))}</h1>
-            <progress value={count / 60}/>
+            <progress style={{width: '95vw', margin: '2%'}} value={smoothCount / 12000}/>
         </div>
     );
 }
